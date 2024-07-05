7b3c2150e1f90ae4c0d6b65da57339700d994ba8
Abhishek Srivastava <123895878+Abhishek75s@users.noreply.github.com>
2024-07-05 18:10:58 +0000

    updated

--- 01_Basics_of_JS/01_varibles.js
+++ 01_Basics_of_JS/01_varibles.js
@@ -3,17 +3,17 @@ var age = 32;
 var age = 56;
 
 console.log(`Age: ${age}`); // op: 56
-    /*
-    var was used before 2015, after ES6 update it depricated
-    because of issue of having multiple declaration of same variable throughout program
-    which often leads ambuiguity in codebase
-    */
-   
-   // ---------------------------------------------
-   
-   // after 2015, ES6 brought let,const feature too.
-   // since then, 'let' became popular,
-   
-   
-   let abhishekAge = 21;
-   console.log(`Abhishek's age is: ${abhishekAge}`);
+/*
+var was used before 2015, after ES6 update it depricated
+because of issue of having multiple declaration of same variable throughout program
+which often leads ambuiguity in codebase
+*/
+
+// ---------------------------------------------
+
+// after 2015, ES6 brought let,const feature too.
+// since then, 'let' became popular,
+
+
+let abhishekAge = 21;
+console.log(`Abhishek's age is: ${abhishekAge}`);
