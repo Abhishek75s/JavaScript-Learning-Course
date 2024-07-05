3b04d6aa44eadb117ca400aa141d2d39444d02d8
Abhishek Srivastava <123895878+Abhishek75s@users.noreply.github.com>
2024-07-05 18:55:13 +0000

    console.table([list of variables])

--- 01_Basics_of_JS/01_variable_concepts.js
+++ 01_Basics_of_JS/01_variable_concepts.js
@@ -1,22 +1,21 @@
-const accountId = 144553
-let accountEmail = "hitesh@google.com"
-var accountPassword = "12345"
-accountCity = "Jaipur"
-let accountState;
+const accId = 222141
+let accEmail = "abhiske@google.com"
+var accPassword = "12345"
+accCity = "Jaunpur"  // also a valid only to initialise a string
+let accState;
 
 // accountId = 2 // not allowed
 
+accEmail = "abc@xyz.com"
+accPassword = "1234"
+accCity = "NOida"
 
-accountEmail = "hc@hc.com"
-accountPassword = "21212121"
-accountCity = "Bengaluru"
-
-console.log(accountId);
+console.log(accId);
 
 /*
 Prefer not to use var
 because of issue in block scope and functional scope
+redundancy of declaration
 */
 
-
-console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+console.table([accId, accEmail, accPassword, accCity, accState]);
